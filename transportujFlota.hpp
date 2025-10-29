68312651fbd89a389625012d4be8798572714c05
Mikołaj Urbański <mikiurb56@gmail.com>
2025-10-29 17:59:12 +0100

    .

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    // Zabezpieczenie przed wywolaniem funkcji z 0;
+    if (towar == 0) return 0;
+
+    Stocznia stocznia{};
+    int      num_of_zaglowiec = 0;
+    int      current_towar    = 0;
+    while(current_towar < towar) {
+        Statek* s = stocznia();
+
+        current_towar = current_towar + s->transportuj();
+
+        if (dynamic_cast< Zaglowiec* >(s)) {
+            num_of_zaglowiec++;
+        }
+
+        delete s;
+    }
+
+    return num_of_zaglowiec;
 }
